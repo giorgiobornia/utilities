e12fe51fe433a98c813e580ab67ad759e7e69d50
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-07 12:14:22 -0600

    Merged all introductory examples

--- 02_C/00_intro_language/00_terminal_stdin_stdout/main.c
+++ 02_C/00_intro_language/00_terminal_stdin_stdout/main.c
@@ -14,27 +14,33 @@ main()
 
 
   /*
-raus:--> gcc -c c0.c    (oggetto: c0.o)
-raus:--> gcc -o c0 c0.o (link: c0)
-raus:--> ./c0           (esecuzione) 
-Ciao mamma!
-raus:-->  
+
+ gcc -c c0.c    (oggetto: c0.o)
+ gcc -o c0 c0.o (link: c0)
+ ./c0           (esecuzione) 
+
+ Ciao mamma!
+  
 [ora inserisco prima solo la seconda, 
  poi solo la terza]
-raus:--> gcc -o c0 c0.c
+
+ gcc -o c0 c0.c
 c0.c:11:10: warning: character constant too 
 long for its type
 c0.c: In function `main':
 c0.c:11: warning: passing arg 1 of `printf' 
 makes pointer from integer without a cast
-raus:--> ./c0
+
+./c0
 Ciao mamma!
 Segmentation fault
-raus:--> gcc -o c0 c0.c
+
+ gcc -o c0 c0.c
 In function 'main':
 : undefined reference to `PRINTF'
 collect2: ld returned 1 exit status
-raus:-->  nm  c0.o
+
+nm  c0.o
 00000000 T main
          U printf
   */
@@ -44,11 +50,12 @@ raus:-->  nm  c0.o
   /*
           Note sul primo programma:
 - main(...): inizio programma
-- \n: manda a capo (nuova linea)
-- C: e' sensibile al maiuscolo <-> minuscolo
-- { ....}: delimita un blocco di istruzioni
-- commento con /* ...
+- main and printf are FUNCTIONS (see later when we talk about functions)
 - printf: funzione per lo standard output
+- \n: manda a capo (nuova linea)
+- C: is case-sensitive
+- { ....}: isolates a block of instructions
+- commento con /* ...
 - ; : terminatore di istruzione
   */
 
