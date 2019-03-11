bfea01fbb790a84076c54d75853b7a735b2e3af2
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-11 15:06:29 -0500

    Removed one recursive call

--- 02_C/00_bad_programming/mistero/main.c
+++ 02_C/00_bad_programming/mistero/main.c
@@ -6,6 +6,10 @@
 int main(int t, int b, char * a)  {
     
 
+ char transl[62] = "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry"; 
+ //31+31 chars, \n is half-way through, from the second part we pick the char to be printed //here is the translation between our encryption alphabet and the human alphabet
+
+
 int return_int;
 
 //the initial value of t is 1
@@ -14,25 +18,21 @@ int return_int;
 if (1 < t) { /* t = 2, 3, ... */
     
 
-if ( t < 3 )     { int b_1 =     main(-86, 0, a+1 ) + a;
-                   int b_2 = a + main(-87, 1 - b, b_1);
-                                 main(-79, -13, b_2 );  } 
+//this t == 2 is reached only at the beginning of each paragraph    
+if ( t == 2 )    { int b_1 =     main(-86,     0, a+1 ) + a;
+                   int b_2 = a + main(-87, 1 - b, b_1 );
+                                 main(-79,   -13, b_2 );  } 
 
 
+//this is reached only at the beginning of the 2nd line of each paragraph, and it loops over the subsequent rows 
  if (t < b )     {  main(t+1, b, a); } //increase t until t == b
 
-
- int mainmin94 = main(-94, -27 + t, a );
+ int b_in = -27 + t;
+ int mainmin94 = main(-94, b_in, a ); 
  
-     if ( mainmin94 && t == 2 && b < 13)  { return_int = main(2, b+1, "%s %d %d\n" ); } //it stays here and increases b until b == 13
-else if ( mainmin94 && t == 2 && b >= 13) { return_int = 9;  }
-else                                      { return_int = 16; }
-
-// return_int = ( a_1, a_2, a_3 );  /*What is this operation? Evaluate the first operand, then discard the result, then evaluate the second operand, ...*/
-
-// return_int =  a_1;
-// return_int =  a_2;
-// return_int =  a_3;
+     if (t == 2 && mainmin94 && b < 13)  { return_int = main(2, b+1, "%s %d %d\n" ); } //it stays here and increases b until b == 13
+else if (t == 2 && mainmin94 && b >= 13) { return_int = 9;  }
+else                                     { return_int = 16; }
 
 }
 
@@ -41,25 +41,29 @@ else if (t < -72) {
 // Here it generates the char to start from in the below -61 main call
 //                       On the /first/second/third/fourth/fifth/sixth/seventh/eighth/ninth/tenth/eleventh/twelfth/ day of Christmas my true love gave to meN/twelve drummers drumming, /eleven pipers piping, /ten lords a-leaping,N/nine ladies dancing, /eight maids a-milking, /seven swans a-swimming,N/six geese a-laying, /five gold rings,N/four calling birds, /three french hens, /two turtle dovesNand /a partridge in a pear treeTNN/
 return_int = main(b, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlw]!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/");  //I think ( terminates, maybe  
+
+//The sign of b decides where to go next    
 }
 
-else if ( -72 <= t && t < -50) { /*if (t != -61) abort();*/ //I observed it reaches here always with this number, not with others. It may come from more than one calling point, though
-                                 while (a[0] != b) { a = a + 1; }  //he compares the ASCII number with the number that you want
-                                 char char_out = a[31];  
-                                 return_int = putchar(char_out);
-}
-/* This putchar is the command that prints to terminal!!! Without this, the program has no output. The calls are such that the char on pos. 31 is always the right one to print to terminal! */
+
 
 else if ( -50 <= t &&  t < 0)               {  while (a[0] != '/') a = a + 1;  return_int = main(t + 1, b , a + 1); }  //this goes where a starts with /
 
 else if ( t == 1 )                          {  return_int = main(2, 2, "%s"); }
 
-else if ( t == 0 )                          {  if (a[0] == '/') { return_int = 1; }
+else if ( t == 0 )                          { int char_that_I_want_encrypted = a[0];
+                                                 if (char_that_I_want_encrypted == '/') { return_int = 1; }
                                                else {
-                                                   int char_that_I_want_to_start_from = a[0];
-                                                   int main_inner = main(-61, char_that_I_want_to_start_from, "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry");  //\n is half-way through, from here I pick the chars to be printed
-                                                   return_int = (0 || main(0, main_inner, a + 1) ); 
-                                            } /*Are both parts of the || evaluated or if the first is true the second is not even evaluated? No, it is short-circuited, going from left to right! */
+                                                   char * a_tmp = &transl[0];
+                                                   
+                                                    while (a_tmp[0] != char_that_I_want_encrypted) { a_tmp = a_tmp + 1; }  //he compares the ASCII number with the number that you want. He decides where is the beginning and takes the 31 component
+                                                    char char_out = a_tmp[31];  
+                                                    putchar(char_out); /* This putchar is the command that prints to terminal!!! Without this, the program has no output. The calls are such that the char on pos. 31 is always the right one to print to terminal! */
+                                 
+                                                    int next = main(0, 307, a + 1);  //increases the char until it finds a /
+                                                    
+                                                   return_int = (0 || next ); 
+                                                } /*Are both parts of the || evaluated or if the first is true the second is not even evaluated? No, it is short-circuited, going from left to right! */
                                             }
 
 return return_int;
