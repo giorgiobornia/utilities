03bb89200a25d685aeb78a7513e22f933fdfb607
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-11 02:16:23 -0500

    Commutativity of && is not valid if you have a recursive call in one of its arguments

--- 02_C/00_bad_programming/mistero/main.c
+++ 02_C/00_bad_programming/mistero/main.c
@@ -18,19 +18,19 @@ if (1 < t) { /* t = 2, 3, ... */
     
  int a_1;   
 
-if ( t < 3 ) { a_1 = main(-79, -13, a + main(-87, 1 - b, main(-86, 0, a+1 ) + a) );  } 
-else         { a_1 = 1; }
+if ( t < 3 )     { a_1 = main(-79, -13, a + main(-87, 1 - b, main(-86, 0, a+1 ) + a) );  } 
+else if (t >= 3) { a_1 = 1; }
 
 
 int a_2;  
 
- if (t < b ) { a_2 = main(t+1, b, a); }
- else        { a_2 = 3; }
+ if (t < b )        { a_2 = main(t+1, b, a); }
+ else  if (t >= b)  { a_2 = 3; }
 
 
  int a_3;
 
-if ( main(-94, -27 + t, a ) && t == 2 ) { a_3 = ( b < 13 ?  main(2, b+1, "%s %d %d\n" ) : 9 ); }
+if ( main(-94, -27 + t, a ) && t == 2 ) { a_3 = ( b < 13 ?  main(2, b+1, "%s %d %d\n" ) : 9 ); }  /*watch out because here commutativity of && is not valid!*/
 else                                    { a_3 = 16; }
 
  
@@ -38,38 +38,25 @@ myout = ( a_1, a_2, a_3 );  /*What is this operation?*/
 
 }
 
-else if (t < 0) { 
- 
-    
-if (t < -72) {
+
+else if (t < -72) {
 
 myout = main(b, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/");
     
 }
  
-else { /* -72 <= t < 0 */
-    
-    
-if (/* -72 <= */ t < -50) {
 
-    
-if (b == *a) {
- char char_out = a[31];
-myout = putchar(char_out); /* This is the command that prints to terminal!!! Without this, the program has no output. The calls are such that the char on pos. 31 is always the right one to print to terminal! */
-}
-else {  myout = main(-65, b , a + 1);  }
-    
-}
-else /* -50 <= t < 0 */ { myout = main((*a == '/') + t, b , a + 1); }
-    
+else if ( -72 <= t && t < -50 && b == a[0]) { char char_out = a[31];  
+                                               myout = putchar(char_out); }
+/* This putchar is the command that prints to terminal!!! Without this, the program has no output. The calls are such that the char on pos. 31 is always the right one to print to terminal! */
 
-}
+else if ( -72 <= t && t < -50 && b != a[0]) {  myout = main(-65, b , a + 1);  }
 
-}
+else if ( -50 <= t &&  t < 0)               {  myout = main((a[0] == '/') + t, b , a + 1); }
 
-else if ( t == 1 )  {  myout = ( main(2, 2, "%s") ); }
+else if ( t == 1 )                          {  myout = ( main(2, 2, "%s") ); }
 
-else /* t == 0 */   {  myout = ( (* a == '/') || main(0, main(-61, *a, "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry"), a+1) ); }
+else /* t == 0 */                           {  myout = ( (a[0] == '/') || main(0, main(-61, a[0], "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry"), a+1) ); }
 
 
 return myout;
