0f55ba2fcf4a03af8785326cf4714eaf536189f6
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-10 21:35:02 -0500

    More dissecting

--- 02_C/00_bad_programming/mystery_dissect.c
+++ 02_C/00_bad_programming/mystery_dissect.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+ /* Operator Precedence: if you open a round parenthesis somewhere the compiler tells you where he expects that to be closed. That is a method to figure out the precedences in a given code */
 
 int main(int t, int _, char * a)
 
@@ -14,12 +15,10 @@ int myout;
     
 if ((! 0) < t)
 {
+    
+ int a_1, a_2, a_3;   
 
-myout = 
-
-( /*if you open a round parenthesis somewhere the compiler tells you where he expects that to be closed. That is a method to figure out the precedences in a given code */
-
-(
+ a_1 = (
 ( t < 3 ) 
 ? 
 main(-79,
@@ -27,29 +26,28 @@ main(-79,
      a + main(-87, 1 - _, main(-86, 0, a+1 ) + a) ) 
 :
 1
-)
-
-, 
-
+);
+ 
+ a_2 = 
 (
 (t < _ ) 
 ? 
 main(t+1, _, a)
 :
 3
-)
+);
 
-,
- 
-(
+a_3 = (
  ( main(-94, -27 + t, a ) && t == 2 ) 
 ?
 ( _ < 13 ?  main(2, _+1, "%s %d %d\n" ) : 9 )
 :
 16
-)
-
 );
+
+ 
+myout = 
+( a_1, a_2, a_3 );
 
 }
 
@@ -57,35 +55,46 @@ else
 
 { 
 
-myout =    
 
-(
     
-(t < 0)
+if (t < 0)
+{
+ 
+    
+if (t < -72) 
+{
 
-? 
+myout = main(_, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/");
 
-( 
-(t < -72) 
-?
-main(_, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/")
-: 
-(
-(t < -50) ? 
-(
+    
+}
+ 
+else {
+    
+    
+if (t < -50)
+{
+myout = (
 (_ == *a)
 ? 
  putchar(a[31]) /* This is the command that prints to terminal!!! Without this, the program has no output. The calls are such that the char on pos. 31 is always the right one to print to terminal! */
 : 
 main(-65, _ , a + 1) 
-)
-: 
-main((*a == '/') + t, _ , a + 1) 
-)
-)
+);
+    
+}
+else { 
+myout = main((*a == '/') + t, _ , a + 1); 
+}
+    
 
-:
+}
 
+}
+
+else {
+
+myout =    
 (
 ( 0 < t ) 
 ? 
@@ -97,10 +106,11 @@ main(0,
      main(-61, *a, "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry"), 
      a+1) 
 )
-)
-
-    
 );
+
+}
+    
+
 
 }
 
