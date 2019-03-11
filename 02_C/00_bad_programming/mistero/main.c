75abccd911f4e5bb2b2f1b16c2d7c8023853b790
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-11 13:21:59 -0500

    Removed some unneeded variables

--- 02_C/00_bad_programming/mistero/main.c
+++ 02_C/00_bad_programming/mistero/main.c
@@ -3,13 +3,7 @@
 
  /* Operator Precedence: if you open a round parenthesis somewhere the compiler tells you where he expects that to be closed. That is a method to figure out the precedences in a given code */
 
-int main(int t, int b, char * a)
-
-// int t;
-// int b;
-// char * a;  //either you declare in the interface, or you declare at the beginning, BEFORE the curly bracket that opens the body of the function.
-
-{
+int main(int t, int b, char * a)  {
     
 
 int return_int;
@@ -19,29 +13,26 @@ int return_int;
 
 if (1 < t) { /* t = 2, 3, ... */
     
- int a_1;   
 
 if ( t < 3 )     { int b_1 =     main(-86, 0, a+1 ) + a;
                    int b_2 = a + main(-87, 1 - b, b_1);
-                       a_1 =     main(-79, -13, b_2 );  } 
-else if (t >= 3) { a_1 = 1; }
+                                 main(-79, -13, b_2 );  } 
 
 
-int a_2;  
-
- if (t < b )        { a_2 = main(t+1, b, a); } //increase t until t == b
- else  if (t >= b)  { a_2 = 3; }
+ if (t < b )     {  main(t+1, b, a); } //increase t until t == b
 
 
- int a_3;
  int mainmin94 = main(-94, -27 + t, a );
-     if ( mainmin94 && t == 2 && b < 13)  { a_3 = main(2, b+1, "%s %d %d\n" ); } 
-else if ( mainmin94 && t == 2 && b >= 13) { a_3 = 9;  }
-else                                      { a_3 = 16; }
-
  
-return_int = ( a_1, a_2, a_3 );  /*What is this operation?*/
-//this always returns 16, except for the last time when it returns 9.. but in debug mode it returns 011...
+     if ( mainmin94 && t == 2 && b < 13)  { return_int = main(2, b+1, "%s %d %d\n" ); } //it stays here and increases b until b == 13
+else if ( mainmin94 && t == 2 && b >= 13) { return_int = 9;  }
+else                                      { return_int = 16; }
+
+// return_int = ( a_1, a_2, a_3 );  /*What is this operation? Evaluate the first operand, then discard the result, then evaluate the second operand, ...*/
+
+// return_int =  a_1;
+// return_int =  a_2;
+// return_int =  a_3;
 
 }
 
@@ -52,7 +43,7 @@ else if (t < -72) {
 return_int = main(b, t, "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l,+,/n{n+,/+#n+,/#;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l q#'+d'K#!/+k#;q#'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# ){nl]!/n{n#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' iwk{KK{nl]!/w{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlw]!/*de}'c ;;{nl'-{}rw]'/+,}##'*}#nc,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# }'+}##(!!/");  //I think ( terminates, maybe  
 }
 
-else if ( -72 <= t && t < -50) { if (t != -61) abort(); //I observed it reaches here always with this number, not with others. It may come from more than one calling point, though
+else if ( -72 <= t && t < -50) { /*if (t != -61) abort();*/ //I observed it reaches here always with this number, not with others. It may come from more than one calling point, though
                                  while (a[0] != b) { a = a + 1; }  //he compares the ASCII number with the number that you want
                                  char char_out = a[31];  
                                  return_int = putchar(char_out);
