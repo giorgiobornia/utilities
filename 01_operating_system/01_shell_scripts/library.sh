34e886173c4c38dc723fc597362edb2cb986236f
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-05-16 17:11:01 -0500

    Draft of renaming from uppercase to lowercase

--- 01_operating_system/01_shell_scripts/library.sh
+++ 01_operating_system/01_shell_scripts/library.sh
@@ -141,3 +141,10 @@ done
 # for file in *; do if [ -d $file ]; then ls $file; fi; done
 
 }
+
+
+
+#translate from uppercase to lowercase
+# for file in *; do if [ -d $file ]; then myvar1=`ls $file`; myvar2=`ls $file | tr '[:upper:]' '[:lower:]'`; cd $file; mv $myvar1 $myvar2; cd ..; fi; done
+
+
