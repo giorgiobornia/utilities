7acef1732997b97f5655a58a33d2a6ac85afbb8a
Giorgio Bornia <giorgio.bornia@ttu.edu>
2020-02-27 19:35:16 -0600

    Almost fixed the shell fixed point

--- 06_math/99_bash/fixed_point.sh
+++ 06_math/99_bash/fixed_point.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# This is the function g(x) for g(x) = x !
 
-  function my_func() { echo c \( $1 \) | bc -l; };  \
-#   function my_func() { echo  \( $1 \) \* \( $1 \) - \( $1 \)   | bc -l; };
+#   function my_func() { echo c \( $1 \) | bc -l; };  \
+  function my_func() { echo  \( $1 \) \* \( $1 \)    | bc -l; };
   
   
   
-  n_max=90;
+  n_max=20;
   x_new=0;
   x_old=0.2;
   i=0;
+  
   eps=0.000001
   err=`echo $eps + 1 | bc`
+
+
   
-while test `echo $err > $eps | bc`  -a  ( $i -lt $n_max ); 
- do 
-  echo Iteration $i; 
-  x_new=`my_func $x_old | bc -l` ;
-  echo Value $x_new; 
-  err=`echo \( $x_old \) - \( $x_new \)  | bc -l`; 
-  echo sqrt\( \( $err \) \* \( $err \) \) | bc -l;  
-  x_old=$x_new;  
-  (( i++ )); 
- done
+ while   test     `echo $err \> $eps | bc`   -a      $i -lt $n_max   ;   # two conditions
+  do 
+    echo Iteration $i; 
+    x_new=`my_func $x_old | bc -l` ;
+    echo Value $x_new; 
+    err=`echo \( $x_old \) - \( $x_new \)  | bc -l`; 
+    echo Error
+    echo sqrt \( \( $err \) \* \( $err \) \) | bc -l;  
+    x_old=$x_new;  
+    (( i++ )); 
+  done
 
